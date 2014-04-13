3f19e993b1dd45d8ec21a89e32cf40d51ceff59e
Lau <fibonacci@zju.edu.cn>
2014-04-13 17:13:58 +0800

    upload Leetcode_permutations.cpp

---
+++ Leetcode_permutations.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>&ans, vector<bool>visited, vector<int>vec, const vector<int>&num)
+    {
+        if(vec.size()==num.size())
+        {
+            ans.push_back(vec);
+            return;
+        }
+        for(int i = 0; i<num.size(); ++i)
+        {
+            if(i && num[i]==num[i-1] && !visited[i-1])
+                continue;
+            if(!visited[i])
+            {
+                visited[i] = true;
+                vec.push_back(num[i]);
+                dfs(ans, visited, vec, num);
+                vec.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int> >ans;
+        if(num.empty())
+            return ans;
+        vector<bool>visited(num.size(), false);
+        vector<int>vec;
+        dfs(ans, visited, vec, num);
+        return ans;
+    }
+};
