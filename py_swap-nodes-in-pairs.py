d730cfb6b83ff23ae28cfea91aa9d2f2821bc65b
axegang <fibonacci@zju.edu.cn>
2014-05-07 16:51:53 +0800

    upload py_swap-nodes-in-pairs.py

---
+++ py_swap-nodes-in-pairs.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param a ListNode
+    # @return a ListNode
+    def swapPairs(self, head):
+        if not head or not head.next: return head
+        p1 = h1 = head
+        p2 = h2 = head.next
+        while p1 and p2:
+            p1.next = p2.next
+            p1 = p1.next
+            if not p1: break
+            p2.next = p1.next
+            p2 = p2.next
+        nxt1 = p1 = h2
+        nxt2 = p2 = h1
+        while p1 and p2:
+            nxt1 = p1.next
+            nxt2 = p2.next
+            p1.next = p2
+            if nxt1:
+                p2.next = nxt1
+            p1 = nxt1
+            p2 = nxt2
+        return h2
+
+        
+        
