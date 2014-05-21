0b26b48f6c483bf8c2eee1284cb8ef4b263193ca
axegang <fibonacci@zju.edu.cn>
2014-05-21 17:10:40 +0800

    add roman-to-integer

---
+++ Leetcode_roman-to-integer.cc
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    inline int c2i(char ch)
+    {
+        switch(ch)
+        {
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+        }
+    }
+    int romanToInt(string s) {
+        int len = s.length(), ans = c2i(s[0]), pre = 0, cur = 0;
+        if(len==1)
+            return ans;
+        for(int i = 1; i<len; ++i)
+        {
+            pre = c2i(s[i-1]);
+            cur = c2i(s[i]);
+            if(pre >= cur)
+                ans += cur;
+            else
+                ans += (cur - 2*pre);
+        }
+        return ans;
+    }
+};
