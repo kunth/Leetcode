539a86e0965d009e796a89f6b40438a27f0f3964
axegang <fibonacci@zju.edu.cn>
2014-05-28 21:22:15 +0800

    update Leetcode_add-two-numbers

--- Leetcode_add-two-numbers.cc
+++ Leetcode_add-two-numbers.cc
@@ -91,3 +91,60 @@ public:
         return head;
     }
 };
+
+
+//SECOND TRIAL
+class Solution {
+public:
+    int cnt_list_node(ListNode* head)
+    {
+        int cnt = 0;
+        while(head)
+        {
+            ++cnt;
+            head = head->next;
+        }
+        return cnt;
+    }
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+        int v = 0, carry = 0;
+        int c1 = cnt_list_node(l1);
+        int c2 = cnt_list_node(l2);
+        if(c1 < c2)
+            swap(l1, l2);
+        ListNode*p = l1, *pre = NULL;
+        while(l2)
+        {
+            pre = p;
+            v = p->val + l2->val + carry;
+            carry = v / 10;
+            v %= 10;
+            p->val = v;
+            p = p->next;
+            l2 = l2->next;
+        }
+        while(carry)
+        {
+            if(p)
+            {
+                pre = p;
+                v = p->val + carry;
+                carry = v/10;
+                v %= 10;
+                p->val = v;
+                p = p->next;
+            }
+            else
+            {
+                ListNode* nd = new ListNode(carry);
+                pre->next = nd;
+                return l1;
+            }
+        }
+        return l1;
+    }
+};
