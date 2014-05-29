8728282e031de8f095468f89208fa1773d386ef7
axegang <fibonacci@zju.edu.cn>
2014-05-29 13:45:50 +0800

    update regular-expression-matching

--- Leetcode_regular-expression-matching.cc
+++ Leetcode_regular-expression-matching.cc
@@ -21,3 +21,28 @@ public:
         }
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if(*p=='\0')
+            return *s =='\0';
+        if(*(p+1)=='*')
+        {
+            if(isMatch(s, p+2))
+                return true;
+            while((*p=='.' && *s!='\0') || (*p==*s))
+            {
+                ++s;
+                if(isMatch(s, p+2))
+                    return true;
+            }
+            return false;
+        }
+        else if((*p=='.' && *s!='\0') ||(*s==*p))
+            return isMatch(s+1, p+1);
+        else
+            return false;
+    }
+};
