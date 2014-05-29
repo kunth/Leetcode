58485aada33f1b6539ca1bc49482768bea87524f
axegang <fibonacci@zju.edu.cn>
2014-05-29 14:24:27 +0800

    update roman-to-integer

--- Leetcode_roman-to-integer.cc
+++ Leetcode_roman-to-integer.cc
@@ -29,3 +29,34 @@ public:
         return ans;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    inline int c2n(char ch)
+    {
+        switch(ch)
+        {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+    int romanToInt(string s) {
+        int ans = 0, last = 0, cur = 0;
+        for(int i = 0; i<s.length(); ++i)
+        {
+            cur = c2n(s[i]);
+            ans += cur;
+            if(i && cur>last)
+                ans -= 2*last;
+            last = cur;
+        }
+        return ans;
+    }
+};
