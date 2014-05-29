c3e226d2a0e4d1ac0c2fa4b94234d3f75c3f403a
axegang <fibonacci@zju.edu.cn>
2014-05-29 14:00:36 +0800

    update container-with-most-water

--- Leetcode_container-with-most-water.cpp
+++ Leetcode_container-with-most-water.cpp
@@ -17,3 +17,20 @@ public:
         return res;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int ans = 0, i = 0, j = height.size()-1;
+        while(i<j)
+        {
+            ans = max(ans, min(height[i], height[j])*(j-i));
+            if(height[i]<height[j])
+                ++i;
+            else
+                --j;
+        }
+        return ans;
+    }
+};
