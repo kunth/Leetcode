6f2f82dfac726308700a67851e77b498c9968106
axegang <fibonacci@zju.edu.cn>
2014-06-02 18:44:59 +0800

    add second trial of valid-parentheses

--- Leetcode_valid-parentheses.cc
+++ Leetcode_valid-parentheses.cc
@@ -39,3 +39,40 @@ public:
         return st.empty() ? true : false;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.empty())
+            return true;
+        stack<char>st;
+        for(int i = 0; i<s.length(); ++i)
+        {
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                st.push(s[i]);
+            else if(s[i]==')')
+            {
+                if(!st.empty() && st.top()=='(')
+                    st.pop();
+                else
+                    return false;
+            }
+            else if(s[i]==']')
+            {
+                if(!st.empty() && st.top()=='[')
+                    st.pop();
+                else
+                    return false;
+            }
+            else if(s[i]=='}')
+            {
+                if(!st.empty() && st.top()=='{')
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
