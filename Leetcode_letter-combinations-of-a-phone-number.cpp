05656b663648fab9f3b041cbdf7cecdae1641fc8
axegang <fibonacci@zju.edu.cn>
2014-06-02 16:32:35 +0800

    add second trial of letter-combinations-of-a-phone-number

--- Leetcode_letter-combinations-of-a-phone-number.cpp
+++ Leetcode_letter-combinations-of-a-phone-number.cpp
@@ -39,3 +39,61 @@ public:
         dfs(ans, "", digits);
     }
 };
+
+//SECOND TRIAL
+class Solution {
+private:
+    vector<string> strvec;
+    void dfs(vector<string>&ans, string& str, string digits)
+    {
+        if(digits.empty())
+        {
+            ans.push_back(str);
+            return;
+        }
+        if(digits[0]<'2')
+            return;
+        string numstr = strvec[digits[0]-'2'];
+        for(int i = 0; i<numstr.length(); ++i)
+        {
+            str += numstr[i];
+            dfs(ans, str, digits.substr(1));
+            str=str.substr(0, str.length()-1);
+        }
+    };
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string>ans;
+        if(digits.empty())
+        {
+            ans.push_back(digits);
+            return ans;
+        }
+        string num, str;
+        for(int i = 2; i<=9; ++i)
+        {
+            num = "";
+            if(i==7 || i==9)
+            {
+                num += strvec.back().back()+1;
+                for(int i = 0; i<=2; ++i)
+                    num += num[i]+1;
+                strvec.push_back(num);
+            }
+            else
+            {
+                if(i==2)
+                    strvec.push_back("abc");
+                else
+                {
+                    num += strvec.back().back()+1;
+                    num += num[0]+1;
+                    num += num[1]+1;
+                    strvec.push_back(num);
+                }
+            }
+        }
+        dfs(ans, str, digits);
+        return ans;
+    }
+};
