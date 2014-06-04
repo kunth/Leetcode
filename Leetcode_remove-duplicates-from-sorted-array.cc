94f40267a6e5f786ddced2dcc4f9d452ca9da706
axegang <fibonacci@zju.edu.cn>
2014-06-04 16:17:09 +0800

    add second trial of remove-duplicates-from-sorted-array

--- Leetcode_remove-duplicates-from-sorted-array.cc
+++ Leetcode_remove-duplicates-from-sorted-array.cc
@@ -20,3 +20,22 @@ public:
         return n-diff;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        if(n<=1)
+            return n;
+        int cnt = 1;
+        for(int i = 1; i<n; ++i)
+        {
+            if(A[i]!=A[cnt-1])
+            {
+                A[cnt] = A[i];
+                ++cnt;
+            }
+        }
+        return cnt;
+    }
+};
