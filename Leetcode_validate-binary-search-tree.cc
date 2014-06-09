0ceace1de2c20bda45a358cc7a97d48d5cbe0446
axegang <fibonacci@zju.edu.cn>
2014-06-09 17:27:34 +0800

    add second trial of validate-binary-search-tree

--- Leetcode_validate-binary-search-tree.cc
+++ Leetcode_validate-binary-search-tree.cc
@@ -34,3 +34,25 @@ public:
         return ans;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+private:
+    vector<int>vec;
+    void InOrder(TreeNode *root)
+    {
+        if(!root)
+            return;
+        InOrder(root->left);
+        vec.push_back(root->val);
+        InOrder(root->right);
+    }
+public:
+    bool isValidBST(TreeNode *root) {
+        InOrder(root);
+        for(int i = 0; i<(int)vec.size()-1; ++i)
+            if(vec[i] >= vec[i+1])
+                return false;
+        return true;
+    }
+};
