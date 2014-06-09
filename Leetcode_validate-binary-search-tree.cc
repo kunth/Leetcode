b8c03a58931e23bcd3df1a5988b1217b3521fc38
axegang <fibonacci@zju.edu.cn>
2014-06-09 19:07:12 +0800

    add thrid trial of validate-binary-search-tree

--- Leetcode_validate-binary-search-tree.cc
+++ Leetcode_validate-binary-search-tree.cc
@@ -56,3 +56,21 @@ public:
         return true;
     }
 };
+
+//THIRD TRIAL
+class Solution {
+private:
+    bool isValid(TreeNode *root, int left, int right)
+    {
+        if(!root)
+            return true;
+        if(left < root->val && root->val < right)
+            return isValid(root->left, left, root->val) && isValid(root->right, root->val, right);
+        else
+            return false;
+    }
+public:
+    bool isValidBST(TreeNode *root) {
+        return isValid(root, INT_MIN, INT_MAX);
+    }
+};
