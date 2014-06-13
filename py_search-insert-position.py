83b0850764e4615484a4b66531b7fb85e0b4a490
axegang <fibonacci@zju.edu.cn>
2014-06-13 16:31:11 +0800

    add python version of search-insert-position

---
+++ py_search-insert-position.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param A, a list of integers
+    # @param target, an integer to be inserted
+    # @return integer
+    def searchInsert(self, A, target):
+        if not A:
+            return 0;
+        left = 0
+        right = len(A)-1
+        while left<=right:
+            mid = (left+right)//2
+            if A[mid] < target:
+                left = mid+1
+            elif A[mid] > target:
+                right = mid-1
+            else:
+                return mid
+        return left
