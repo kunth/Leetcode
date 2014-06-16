2873202d0e2ea60eb3398f4249dc3323232fc0db
axegang <fibonacci@zju.edu.cn>
2014-06-16 20:03:40 +0800

    update .gitignore and second trial of max-points-on-a-line

--- Leetcode_max-points-on-a-line.cc
+++ Leetcode_max-points-on-a-line.cc
@@ -40,3 +40,44 @@ public:
         return ans;
     }
 };
+
+//SECOND TRIAL, almost the same
+class Solution {
+public:
+    int maxPoints(vector<Point> &points) {
+        if(points.empty())
+            return 0;
+        else if(points.size()<=2)
+            return points.size();
+        int same = 0, vertical = 0, slope_cnt = 0, ans = 0;
+        float slope;
+        unordered_map<float, int>mp;
+        for(int i = 0; i<points.size(); ++i)
+        {
+            same = vertical = slope_cnt = 0;
+            mp.clear();
+            for(int j = 0; j<points.size(); ++j)
+            {
+                if(j != i)
+                {
+                    if(points[j].x == points[i].x)
+                    {
+                        if(points[j].y==points[i].y)
+                            ++same;
+                        else
+                            ++vertical;
+                    }
+                    else
+                    {
+                        slope = float(points[j].y - points[i].y) / (points[j].x - points[i].x);
+                        ++mp[slope];
+                        if(mp[slope] > slope_cnt)
+                            slope_cnt = mp[slope];
+                    }
+                }
+            }
+            ans = max(ans, 1+same+max(slope_cnt, vertical));
+        }
+        return ans;
+    }
+};
