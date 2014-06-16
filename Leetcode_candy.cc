99f50dbeb9c0ffe89c2a66982cb1a10faed658d4
axegang <fibonacci@zju.edu.cn>
2014-06-16 21:49:20 +0800

    update second trial of candy

--- Leetcode_candy.cc
+++ Leetcode_candy.cc
@@ -21,3 +21,24 @@ public:
         return accumulate(vec.begin(), vec.end(), 0);
     }
 };
+
+//SECOND TRIAL, almost the same
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        if(ratings.empty())
+            return 0;
+        vector<int>vec(ratings.size(), 0);
+        vec[0] = vec[vec.size()-1] = 1;
+        for(int i=0; i<ratings.size()-1; ++i)
+            if(ratings[i]<ratings[i+1])
+                vec[i+1] = vec[i]+1;
+            else
+                vec[i+1] = 1;
+        for(int i=ratings.size()-2; i>=0; --i)
+            if(ratings[i]>ratings[i+1])
+                vec[i] = max(vec[i], 1+vec[i+1]);
+
+        return accumulate(vec.begin(), vec.end(), 0);
+    }
+};
