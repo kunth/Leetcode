3065d1e2bc545845c769d76963937ebc78a724ea
axegang <fibonacci@zju.edu.cn>
2014-06-17 13:14:35 +0800

    add second trial of best-time-to-buy-and-sell-stock-ii

--- Leetcode_best-time-to-buy-and-sell-stock-ii.cc
+++ Leetcode_best-time-to-buy-and-sell-stock-ii.cc
@@ -19,3 +19,19 @@ public:
         return res;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if(prices.size()<=1)
+            return 0;
+        int ans = 0;
+        for(int i = 0; i<prices.size()-1; ++i)
+        {
+            if(prices[i]<prices[i+1])
+                ans += (prices[i+1]-prices[i]);
+        }
+        return ans;
+    }
+};
