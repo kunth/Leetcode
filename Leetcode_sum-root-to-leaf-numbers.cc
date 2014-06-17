642a8cc8a59aba728222794efddd0a4de560326f
axegang <fibonacci@zju.edu.cn>
2014-06-17 10:16:07 +0800

    add second trial of sum-root-to-leaf-numbers

--- Leetcode_sum-root-to-leaf-numbers.cc
+++ Leetcode_sum-root-to-leaf-numbers.cc
@@ -34,3 +34,45 @@ public:
         return ret;
     }
 };
+
+//SECOND TRIAL, the first one is better, obviously.
+class Solution {
+private:
+    void dfs(int&ans, vector<int>&vec, TreeNode*root)
+    {
+        if(!root->left && !root->right)
+        {
+            int sum = 0, ratio = 1;
+            for(int i = vec.size()-1; i>=0; --i)
+            {
+                sum += ratio * vec[i];
+                ratio *= 10;
+            }
+            ans += sum;
+            return;
+        }
+        if(root->left)
+        {
+            vec.push_back(root->left->val);
+            dfs(ans, vec, root->left);
+            vec.pop_back();
+            
+        }
+        if(root->right)
+        {
+            vec.push_back(root->right->val);
+            dfs(ans, vec, root->right);
+            vec.pop_back();
+        }
+    }
+public:
+    int sumNumbers(TreeNode *root) {
+        if(!root)
+            return 0;
+        int ans = 0;
+        vector<int>vec;
+        vec.push_back(root->val);
+        dfs(ans, vec, root);
+        return ans;
+    }
+};
