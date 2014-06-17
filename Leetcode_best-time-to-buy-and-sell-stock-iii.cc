d3075a4c6c8ada8ed8a77151c4ffe0aef3fb571e
axegang <fibonacci@zju.edu.cn>
2014-06-17 13:55:39 +0800

    add best-time-to-buy-and-sell-stock-iii

--- Leetcode_best-time-to-buy-and-sell-stock-iii.cc
+++ Leetcode_best-time-to-buy-and-sell-stock-iii.cc
@@ -25,3 +25,29 @@ public:
         return ans;
     }
 };
+
+//SECOND TRIAL, 60ms
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if(prices.size()<=1)
+            return 0;
+        vector<int>v1(prices.size(), 0), v2(prices.size(), 0);
+        int low = prices[0], high = prices[prices.size()-1];
+        for(int i = 1; i<prices.size(); ++i)
+        {
+            low = min(low, prices[i]);
+            v1[i] = max(v1[i-1], prices[i] - low);
+        }
+            
+        for(int j = prices.size()-2; j>=0; --j)
+        {
+            high = max(high, prices[j]);
+            v2[j] = max(v2[j+1], high - prices[j]);
+        }
+        int ans = 0;
+        for(int i = 0; i<prices.size(); ++i)
+            ans = max(ans, v1[i]+v2[i]);
+        return ans;
+    }
+};
