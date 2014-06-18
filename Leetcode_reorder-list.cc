9ed8a6382491d720a467890f241a6e6c49bef168
axegang <fibonacci@zju.edu.cn>
2014-06-18 16:29:03 +0800

    update second trial of reorder-list

--- Leetcode_reorder-list.cc
+++ Leetcode_reorder-list.cc
@@ -49,3 +49,50 @@ public:
         }
     }
 };
+
+//SECOND TRIAL
+class Solution {
+private:
+    ListNode* reverseList(ListNode* head)
+    {
+        if(!head || !head->next)
+            return head;
+        ListNode *p = head, *pre = NULL, *nxt = NULL;
+        while(p)
+        {
+            nxt = p->next;
+            p->next = pre;
+            pre = p;
+            p = nxt;
+        }
+        return pre;
+    }
+public:
+    void reorderList(ListNode *head) {
+        if(!head || !head->next)
+            return;
+        ListNode*p1 = head, *p2 = head, *pre = NULL, *nxt1 = NULL, *nxt2 = NULL;
+        while(p1)
+        {
+            p1 = p1->next;
+            pre = p2;
+            if(p1)
+            {
+                p1 = p1->next;
+                p2 = p2->next;
+            }
+        }
+        p2 = pre->next;
+        pre->next = NULL;
+        p2 = reverseList(p2);
+        p1 = head;
+        while(p1 && p2)
+        {
+            nxt1 = p1->next;
+            nxt2 = p2->next;
+            p1->next = p2;
+            p1 = p2->next = nxt1;
+            p2 = nxt2;
+        }
+    }
+};
