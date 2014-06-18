c6d3705d0abfa95515b66f54158e9ef8694bafaf
axegang <fibonacci@zju.edu.cn>
2014-06-18 21:16:55 +0800

    update second trial of valid-palindrome

--- Leetcode_valid-palindrome.cpp
+++ Leetcode_valid-palindrome.cpp
@@ -16,3 +16,23 @@ public:
         return rev_str == str;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty())
+            return true;
+        int cnt = 0;
+        for(int i = 0; i<s.length(); ++i)
+        {
+            if((s[i]>='0' && s[i]<='9')||(s[i]>='a' &&s[i]<='z'))
+                s[cnt++] = s[i];
+            else if(s[i]>='A' && s[i]<='Z')
+                s[cnt++] = s[i] + 'a' - 'A';
+        }
+        s = s.substr(0, cnt);
+        string revs(s.rbegin(), s.rend());
+        return revs==s;
+    }
+};
