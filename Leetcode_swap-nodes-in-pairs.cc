5f5a9e5094e59c1a62ca173516668e300a4c78a2
axegang <fibonacci@zju.edu.cn>
2014-06-18 12:45:48 +0800

    update second trial of swap-nodes-in-pairs

--- Leetcode_swap-nodes-in-pairs.cc
+++ Leetcode_swap-nodes-in-pairs.cc
@@ -29,3 +29,36 @@ public:
         return head;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    ListNode *swapPairs(ListNode *head) {
+        if(!head || !head->next)
+            return head;
+        ListNode *l1 = head, *l2 = head->next;
+        ListNode *p1 = l1, *p2 = l2, *nxt1 = NULL, *nxt2 = NULL;
+        while(p1 && p2)
+        {
+            p1 = p1->next = p2->next;
+            if(p1)
+                p2 = p2->next = p1->next;
+        }
+        p1 = l1;
+        p2 = l2;
+        while(p1 && p2)
+        {
+            nxt1 = p1->next;
+            nxt2 = p2->next;
+            p2->next = p1;
+            if(nxt2)
+            {
+                p2 = p1->next = nxt2;
+                p1 = nxt1;
+            }
+            else
+                break;
+        }
+        return l2;
+    }
+};
