0d3f0de4a446e754e035ef57d989f54f855c79ea
axegang <fibonacci@zju.edu.cn>
2014-06-18 14:01:02 +0800

    add second trial of reverse-nodes-in-k-group

--- Leetcode_reverse-nodes-in-k-group.cc
+++ Leetcode_reverse-nodes-in-k-group.cc
@@ -56,3 +56,41 @@ public:
         return newHead;
     }
 };
+
+//SECOND TRIAL, without extra space
+class Solution {
+public:
+    ListNode *reverseKGroup(ListNode *head, int k) {
+        int len = 0;
+        ListNode* p = head, *pre = NULL, *h = NULL, *nxt = NULL, *lastp = NULL, *curp = NULL;
+        while(p)
+        {
+            ++len;
+            p = p->next;
+        }
+        if(len < k || k<=1)
+            return head;
+        p = head;
+        while(len >= k)
+        {
+            len -= k;
+            curp = p;
+            for(int i = 0; i<k; ++i)
+            {
+                nxt = p->next;
+                p->next = pre;
+                pre = p;
+                p = nxt;
+            }
+            if (!h)
+                h = pre;
+            else
+                lastp->next = pre;
+            lastp = curp;
+            pre = NULL;
+        }
+        if(p)
+            lastp->next = p;
+        return h;
+    }
+};
