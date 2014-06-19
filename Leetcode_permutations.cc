8ef8aa4684286e9948226d11250508259346fcbf
axegang <fibonacci@zju.edu.cn>
2014-06-19 20:23:46 +0800

    update second trial of permutation

--- Leetcode_permutations.cc
+++ Leetcode_permutations.cc
@@ -31,3 +31,39 @@ public:
         return ans;
     }
 };
+
+//SECOND TRIAL, almost the same
+class Solution {
+private:
+    vector<int>num;
+    void dfs(vector<vector<int> >&ans, vector<int>vec, vector<bool>visited)
+    {
+        if(vec.size()==num.size())
+        {
+            ans.push_back(vec);
+            return;
+        }
+        for(int i = 0; i<num.size(); ++i)
+        {
+            if(!visited[i])
+            {
+                visited[i] = true;
+                vec.push_back(num[i]);
+                dfs(ans, vec, visited);
+                vec.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+        vector<vector<int> >ans;
+        if(num.empty())
+            return ans;
+        this->num = num;
+        vector<int>vec;
+        vector<bool>visited(num.size(), false);
+        dfs(ans, vec, visited);
+        return ans;
+    }
+};
