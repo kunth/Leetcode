da3cf9ae1cf48a6356a2c72e121dfebf13e5f55a
axegang <fibonacci@zju.edu.cn>
2014-06-20 11:21:12 +0800

    update second trial of first-missing-positive

--- Leetcode_first-missing-positive.cc
+++ Leetcode_first-missing-positive.cc
@@ -18,3 +18,34 @@ public:
         return j;
     }
 };
+
+//SECOND TRIAL, O(N) running time with constant space
+class Solution {
+public:
+    int firstMissingPositive(int A[], int n) {
+        int k = 0;
+        while(k<n)
+        {
+            if(A[k]>0 && A[k]<=n)
+            {
+                if(A[k]<=k+1)
+                {
+                    A[A[k]-1] = A[k];
+                    ++k;
+                }
+                else if(A[A[k]-1]<=0 || A[A[k]-1]>n)
+                    A[A[k]-1] = A[k];
+                else if(A[A[k]-1]!=A[k])
+                    swap(A[k], A[A[k]-1]);
+                else
+                    ++k;
+            }
+            else
+                ++k;
+        }
+        for(int i = 0; i<n; ++i)
+            if(A[i]!=i+1)
+                return i+1;
+        return n+1;
+    }
+};
