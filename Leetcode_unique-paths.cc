eea1d90f59c9a21c8841f4a60e59b5a2a367d3d4
axegang <fibonacci@zju.edu.cn>
2014-06-20 16:55:24 +0800

    udpate second trial of unique-paths

--- Leetcode_unique-paths.cc
+++ Leetcode_unique-paths.cc
@@ -21,3 +21,25 @@ public:
         return A[m][n];
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(n==1 || m==1)
+            return 1;
+        int** grid = new int*[m];
+        for(int i = 0; i<m; ++i)
+            grid[i] = new int[n];
+        for(int i = 0; i<m; ++i)
+            grid[i][0] = 1;
+        for(int i = 0; i<n; ++i)
+            grid[0][i] = 1;
+        for(int i = 1; i<m; ++i)
+            for(int j = 1; j<n; ++j)
+                grid[i][j] = grid[i-1][j] + grid[i][j-1];
+        int ans = grid[m-1][n-1];
+        delete []grid;
+        return ans;
+    }
+};
