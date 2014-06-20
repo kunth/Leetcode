f60f4ab026ce782424f423c09b5035c79d5f31c7
axegang <fibonacci@zju.edu.cn>
2014-06-20 19:24:53 +0800

    update second trial of rm-dup-from-sorted-list

--- Leetcode_remove-duplicates-from-sorted-list.cc
+++ Leetcode_remove-duplicates-from-sorted-list.cc
@@ -8,31 +8,6 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    ListNode *deleteDuplicates(ListNode *head) {
-        map<int, int> dmap;
-        if(head)
-        {
-            ListNode* p = head, *pdel = head->next;
-            dmap[p->val] = 1;
-            while(pdel)
-            {
-                if(dmap[pdel->val]==1)
-                {
-                    p->next = pdel->next;
-                }
-                else
-                {
-                    dmap[pdel->val] = 1;
-                    p = pdel;
-                }
-                pdel = pdel->next;
-            }
-        }
-        return head;
-    }
-};
 
 //SECOND TRIAL
 class Solution {
@@ -41,14 +16,10 @@ public:
         if(!head)
             return head;
         ListNode *pre = head, *p = head->next;
-        int last = head->val;
         while(p)
         {
-            if(p->val != last)
-            {
-                last = p->val;
+            if(p->val != pre->val)
                 pre = p;
-            }
             else
                 pre->next = p->next;
             p = p->next;
