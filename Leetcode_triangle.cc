07bf9ed9957ea20c0bf2afda886e38d853f98df5
axegang <fibonacci@zju.edu.cn>
2014-06-21 16:17:18 +0800

    add second trial of triangle

--- Leetcode_triangle.cc
+++ Leetcode_triangle.cc
@@ -24,3 +24,29 @@ public:
         return ans;
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    int minimumTotal(vector<vector<int> > &triangle) {
+        if(triangle.empty())
+            return 0;
+        int sz = triangle.size();
+        for(int i = 1; i<sz; ++i)
+        {
+            for(int j = 0; j<=i; ++j)
+            {
+                if(j==0)
+                    triangle[i][j] += triangle[i-1][0];
+                else if(j==i)
+                    triangle[i][j] += triangle[i-1][i-1];
+                else
+                    triangle[i][j] += min(triangle[i-1][j-1], triangle[i-1][j]);
+            }
+        }
+        int ans = triangle[sz-1][0];
+        for(int i = 1; i<sz; ++i)
+            ans = min(ans, triangle[sz-1][i]);
+        return ans;
+    }
+};
