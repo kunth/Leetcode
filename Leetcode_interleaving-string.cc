4ad93adbd2bd6298eab16145a4469b7a160eb426
axegang <fibonacci@zju.edu.cn>
2014-06-23 21:27:18 +0800

    updaste second trial of interleaving-string

--- Leetcode_interleaving-string.cc
+++ Leetcode_interleaving-string.cc
@@ -30,3 +30,33 @@ public:
         return dp[len1][len2];
     }
 };
+
+//SECOND TRIAL, almost the same
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int l1 = s1.length(), l2 = s2.length();
+        if(l1+l2 != s3.length())
+            return false;
+        if(!l1)
+            return s2==s3;
+        else if(!l2)
+            return s1==s3;
+        vector<vector<bool> >mat;
+        for(int i = 0; i<=l1; ++i)
+        {
+            vector<bool>v(l2+1, false);
+            mat.push_back(v);
+        }
+        for(int i = 1; i<=l1; ++i)
+            mat[i][0] = (s1[i-1] == s3[i-1]);
+        for(int i = 1; i<=l2; ++i)
+            mat[0][i] = (s2[i-1] == s3[i-1]);
+        for(int i = 1; i<=l1; ++i)
+        {
+            for(int j = 1; j<=l2; ++j)
+                mat[i][j] = (mat[i-1][j] && s1[i-1]==s3[i+j-1]) || (mat[i][j-1] && s2[j-1]==s3[i+j-1]);
+        }
+        return mat[l1][l2];
+    }
+};
