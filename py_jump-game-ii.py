17f3afdea9ee0a32f5ae2d381d82aa515cf1ff65
axegang <fibonacci@zju.edu.cn>
2014-06-24 11:33:45 +0800

    update second trial of python of jump-game-ii

--- py_jump-game-ii.py
+++ py_jump-game-ii.py
@@ -13,3 +13,27 @@ class Solution:
                     if farthest >= len(A)-1:
                         return ans
             cur = nxt
+
+#SECOND TRAIL
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    def jump(self, A):
+        if len(A)==1:
+            return 0
+        if A[0]>=len(A)-1:
+            return 1
+        begin = 0
+        end = A[0]
+        step = 0
+        ans = 1
+        while 2:
+            while begin<=end:
+                step = max(step, A[begin]+begin)
+                begin += 1
+            if step>=len(A)-1:
+                return 1+ans
+            begin = end
+            end = step
+            ans += 1
+            step = 0
