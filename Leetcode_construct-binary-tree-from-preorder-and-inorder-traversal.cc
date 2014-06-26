a85916717fc9391ffbb9cf8a4b3f9606e1f4eda3
axegang <fibonacci@zju.edu.cn>
2014-06-26 18:37:40 +0800

    add second trial

--- Leetcode_construct-binary-tree-from-preorder-and-inorder-traversal.cc
+++ Leetcode_construct-binary-tree-from-preorder-and-inorder-traversal.cc
@@ -1,5 +1,14 @@
- vector<int>prevec, invec;
- 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int>prevec, invec;
+
  TreeNode* Construct(int pre_pos, int in_left, int in_right)
  {
      if(pre_pos>=prevec.size() || in_left>in_right)
@@ -32,7 +41,7 @@
      }
      return p;
  }
- 
+
 class Solution {
 public:
     TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
@@ -41,5 +50,27 @@ public:
         prevec = preorder;
         invec = inorder;
         return Construct(0, 0, preorder.size()-1);
+    }
+};
+
+//SECOND TRIAL
+class Solution {
+private:
+    TreeNode *construct(int pre[], int in[], int len)
+    {
+        if(len<=0)
+            return NULL;
+        int root_val = pre[0], offset = 0;
+        TreeNode* root = new TreeNode(root_val);
+        while(in[offset] != root_val)
+            ++offset;
+        root->left = construct(pre+1, in, offset);
+        root->right = construct(pre+offset+1, in+offset+1, len-offset-1);
+        return root;
+    }
+public:
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        int *pre = &preorder[0], *in = &inorder[0];
+        return construct(pre, in, preorder.size());
     }
 };
