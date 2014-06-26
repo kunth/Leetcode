33c28f935229af6e018ab7f5a5b08c07fe6c3189
axegang <fibonacci@zju.edu.cn>
2014-06-26 17:14:26 +0800

    update second trial

--- Leetcode_construct-binary-tree-from-inorder-and-postorder-traversal.cc
+++ Leetcode_construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -45,3 +45,29 @@ public:
         postvec = postorder;
         return construct_tree(0, invec.size()-1);
     }
+}
+
+//SECOND TRIAL, 116ms
+class Solution {
+private:
+    TreeNode *construct(int in[], int post[], int len)
+    {
+        if(len<=0)
+            return NULL;
+        int root_val = post[len-1];
+        TreeNode* root = new TreeNode(root_val);
+        int offset = 0;
+        while(offset<len && in[offset]!=root_val)
+            ++offset;
+        root->left = construct(in, post, offset);
+        root->right = construct(in+offset+1, post+offset, len-1-offset);
+        return root;
+    }
+public:
+    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        if(inorder.empty())
+            return NULL;
+        int *in = &inorder[0], *post = &postorder[0];
+        return construct(in, post, inorder.size());
+    }
+};
