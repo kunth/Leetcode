906e9317a43e99561902c0d76b06a6968565dde7
axegang <fibonacci@zju.edu.cn>
2014-06-26 09:21:24 +0800

    update second trial of minimum-path-sum

--- Leetcode_minimum-path-sum.cc
+++ Leetcode_minimum-path-sum.cc
@@ -30,3 +30,28 @@ public:
         return grid[row-1][col-1];
     }
 };
+
+//SECOND TRIAL
+class Solution {
+public:
+    int minPathSum(vector<vector<int> > &grid) {
+        if(grid.empty())
+            return 0;
+        int row = grid.size(), col = grid[0].size();
+        for(int i=0; i<row; ++i)
+        {
+            for(int j=0; j<col; ++j)
+            {
+                if(!i && !j)
+                    continue;
+                else if(!j)
+                    grid[i][j] += grid[i-1][j];
+                else if(!i)
+                    grid[i][j] += grid[i][j-1];
+                else
+                    grid[i][j] += min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[row-1][col-1];
+    }
+};
