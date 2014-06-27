7e82fccd0d478b68d96fd0d011ec3a7c49835ffd
axegang <fibonacci@zju.edu.cn>
2014-06-27 22:57:37 +0800

    add Solution TWO of merge-sorted-array

--- Leetcode_merge-sorted-array.cc
+++ Leetcode_merge-sorted-array.cc
@@ -27,3 +27,30 @@ public:
         //}
     }
 };
+
+//SOLUTION TWO, which is better.
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        if(!n)
+            return;
+        if(!m)
+        {
+            for(int i = 0; i<n; ++i)
+                A[i] = B[i];
+            return;
+        }
+        int i = m-1, j = n-1;
+        while(i>=0 && j>=0)
+        {
+            if(A[i]>=B[j])
+                A[i+j+1] = A[i--];
+            else
+                A[i+j+1] = B[j--];
+        }
+        if(j<0)
+            return;
+        for(; j>=0; --j)
+            A[j] = B[j];
+    }
+};
