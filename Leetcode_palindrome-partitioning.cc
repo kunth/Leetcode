2476f9fe279ec91030fbd68ff5d4eee8950df793
Jun Lau <fibonacci@zju.edu.cn>
2014-08-04 23:56:38 +0800

    update the format

--- Leetcode_palindrome-partitioning.cc
+++ Leetcode_palindrome-partitioning.cc
@@ -5,30 +5,30 @@ public:
         if(s.empty())
         {
 			if(tmpstr.empty())
-			    ans.push_back(vec);
+			ans.push_back(vec);
 			return;
-		}
-		tmpstr += s.substr(0, 1);
-		if(tmpstr.length()==1)
+	}
+	tmpstr += s.substr(0, 1);
+	if(tmpstr.length()==1)
+	{
+		vec.push_back(s.substr(0, 1));
+		dfs(ans, vec, "", s.substr(1));
+		vec.pop_back();
+		dfs(ans, vec, tmpstr, s.substr(1));
+	}
+	else
+	{
+		string str = tmpstr;
+		reverse(tmpstr.begin(), tmpstr.end());
+		if(str==tmpstr)
 		{
-			vec.push_back(s.substr(0, 1));
+			vec.push_back(tmpstr);
 			dfs(ans, vec, "", s.substr(1));
 			vec.pop_back();
-			dfs(ans, vec, tmpstr, s.substr(1));
 		}
-		else
-		{
-			string str = tmpstr;
-			reverse(tmpstr.begin(), tmpstr.end());
-			if(str==tmpstr)
-			{
-				vec.push_back(tmpstr);
-				dfs(ans, vec, "", s.substr(1));
-				vec.pop_back();
-			}
-			dfs(ans, vec, str, s.substr(1));
-		}
+		dfs(ans, vec, str, s.substr(1));
 	}
+}
 
 	vector<vector<string>> partition(string s) {
 		vector<vector<string>>ans;
