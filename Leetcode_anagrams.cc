39a9b87ab4b23aa8533303c2708411d9e708694b
Jun Liu <fibonacci@zju.edu.cn>
2014-08-31 16:38:21 +0800

    update the second trial of anagrams

--- Leetcode_anagrams.cc
+++ Leetcode_anagrams.cc
@@ -23,3 +23,28 @@ public:
         return ans;
     }
 };
+
+//Second trial, almost the same
+class Solution {
+public:
+    vector<string> anagrams(vector<string> &strs) {
+        vector<string>ans;
+        if(strs.size()<=1)
+            return ans;
+        map<string, vector<string> >M;
+        string word;
+        vector<string> vec;
+        for(int i = 0; i<strs.size(); ++i) {
+            word = strs[i];
+            sort(word.begin(), word.end());
+            if(M[word].empty()) {
+            }
+            M[word].push_back(strs[i]);
+        }
+        for(auto it = M.begin(); it != M.end(); ++it) {
+            if(it->second.size() > 1)
+                ans.insert(ans.end(), it->second.begin(), it->second.end());
+        }
+        return ans;
+    }
+};
