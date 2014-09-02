d1bab2ffb3828c27318b3299b3d6e4aba6bd7ee5
Jun Liu <fibonacci@zju.edu.cn>
2014-09-02 15:05:44 +0800

    update second solution for longest-common-prefix

--- Leetcode_longest-common-prefix.cc
+++ Leetcode_longest-common-prefix.cc
@@ -35,3 +35,28 @@ public:
         return ret;
     }
 };
+
+
+// Second Solution
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        string ret;
+        if(strs.size() < 1)
+            return ret;
+        int idx = 0;
+        char ch;
+        while(2) {
+            if(idx == strs[0].length()) {
+                return idx ? strs[0].substr(0, idx) : "";
+            }
+            ch = strs[0][idx];
+            for(int i = 1; i<strs.size(); ++i) {
+                if(strs[i].length() == idx || strs[i][idx] != ch) {
+                    return idx ? strs[0].substr(0, idx) : "";
+                }
+            }
+            ++idx;
+        }
+    }
+};
