fdd44da8a65ac84af24e4074d3bd29ca0063505c
Jun Liu <fibonacci@zju.edu.cn>
2014-09-03 22:49:35 +0800

    update the second trial of insert-interval

--- Leetcode_insert-interval.cc
+++ Leetcode_insert-interval.cc
@@ -34,3 +34,30 @@ public:
         return ans;
     }
 };
+
+
+//Second trail, almost the same
+bool cmp(const Interval& lhs, const Interval& rhs){
+    if(lhs.start == rhs.start) {
+        return lhs.end < rhs.end;
+    }
+    return lhs.start < rhs.start;
+}
+ 
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
+        vector<Interval>ans;
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end(), cmp);
+        ans.push_back(intervals[0]);
+        for(int i = 1; i < intervals.size(); ++i) {
+            if(intervals[i].start <= ans.back().end) {
+                ans[ans.size()-1].end = max(intervals[i].end, ans.back().end);
+            } else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
