7b2cf69bb789c3b948d70372127cef1ace1109f2
axegang <fibonacci@zju.edu.cn>
2014-09-09 13:02:47 +0800

    update second solution of valid-sudoku

--- Leetcode_valid-sudoku.cc
+++ Leetcode_valid-sudoku.cc
@@ -23,7 +23,7 @@ public:
             left_row = row;
             right_row = row+2;
         }
-       
+
         if(col % 3 ==2)
         {
             right_col = col;
@@ -49,7 +49,7 @@ public:
     bool isValidSudoku(vector<vector<char> > &board) {
         if(board.empty() || board.size()!=9)
             return false;
-       
+
         for(int i = 0; i<board.size(); ++i)
         {
             for(int j = 0; j<board[i].size(); ++j)
@@ -57,6 +57,73 @@ public:
                 if(board[i][j]!='.' && !is_fill(board, i, j, board[i][j]))
                 {
                     return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+
+//Solution Two
+
+class Solution {
+private:
+    bool rowArr[9], colArr[9], nineGrid[9];
+    bool checkRow(vector<vector<char> > &board, int row){
+        if(rowArr[row])
+            return true;
+        vector<bool>vec(10, false);
+        for(int i = 0; i<9; ++i) {
+            if(board[row][i]=='.')
+                    continue;
+            if(vec[board[row][i]-'0'])
+                return false;
+            vec[board[row][i]-'0'] = true;
+        }
+        rowArr[row] = true;
+        return true;
+    }
+    bool checkCol(vector<vector<char> > &board, int col) {
+        if(colArr[col])
+            return true;
+        vector<bool>vec(10, false);
+        for(int i = 0; i<9; ++i) {
+            if(board[i][col]=='.')
+                    continue;
+            if(vec[board[i][col]-'0'])
+                return false;
+            vec[board[i][col]-'0'] = true;
+        }
+        colArr[col] = true;
+        return true;
+    }
+    bool checkNineGrid(vector<vector<char> > &board, int row, int col) {
+        int cnt = (row / 3)*3 + col / 3;
+        if(nineGrid[cnt])
+            return true;
+        vector<bool>vec(10, false);
+        for(int i = row / 3 * 3; i <= row / 3 * 3  + 2; ++i) {
+            for(int j = col / 3 * 3; j <= col / 3 * 3 + 2; ++j) {
+                if(board[i][j]=='.')
+                    continue;
+                if(vec[board[i][j]-'0'])
+                    return false;
+                vec[board[i][j]-'0'] = true;
+            }
+        }
+        return true;
+    }
+public:
+    bool isValidSudoku(vector<vector<char> > &board) {
+        memset(rowArr, false, sizeof(rowArr));
+        memset(colArr, false, sizeof(colArr));
+        memset(nineGrid, false, sizeof(nineGrid));
+        for(int i = 0; i<9; ++i) {
+            for(int j = 0; j<9; ++j) {
+                if(board[i][j]!='.') {
+                    if(!checkRow(board, i) || !checkCol(board, j) || !checkNineGrid(board, i, j))
+                        return false;
                 }
             }
         }
