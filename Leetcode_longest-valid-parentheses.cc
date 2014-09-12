23264a8bf828d24cba79d8c07ff8d4faf059babe
axegang <fibonacci@zju.edu.cn>
2014-09-12 16:23:06 +0800

    better solution of longest-valid-parentheses

--- Leetcode_longest-valid-parentheses.cc
+++ Leetcode_longest-valid-parentheses.cc
@@ -34,3 +34,41 @@ public:
         return ans;
     }
 };
+
+//Solution Two, O(N) time
+//Use vector to store the current '(' which need to be matched, instead of searching backward
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if(s.empty())
+            return 0;
+        stack<char>st;
+        vector<bool>flag(s.length(), false);
+        vector<int>vec;
+        int ans = 0, cur = 0;
+        for(int i = 0; i<s.length(); ++i) {
+            if(s[i]=='(') {
+                st.push(s[i]);
+                vec.push_back(i);
+            } else {
+                if(!st.empty()) {
+                    if(st.top()=='(') {
+                        st.pop();
+                        flag[i] = flag[vec.back()] = true;
+                        vec.pop_back();
+                    }
+                }
+            }
+        }
+        for(int i = 0; i<s.length(); ++i) {
+            if(flag[i]) {
+                ++cur;
+            } else {
+                ans = max(ans, cur);
+                cur = 0;
+            }
+        }
+        ans = max(ans, cur);
+        return ans;
+    }
+};
