0c352fd3cd8297777e22cfde24f7577753ab0916
Jun Liu <fibonacci@zju.edu.cn>
2014-10-07 10:28:43 +0800

    update text-justification for second trial

--- Leetcode_text-justification.cc
+++ Leetcode_text-justification.cc
@@ -63,3 +63,55 @@ public:
         return ans;
     }
 };
+
+
+//Second trial
+class Solution {
+public:
+    vector<string> fullJustify(vector<string> &words, int L) {
+        vector<string>ans;
+        if(words.empty())
+            return ans;
+        int i = 0, cnt = 0, j = 0, sz = 0, left = 0, quotient = 0;
+        while(i<words.size()) {
+            cnt = words[i].length();
+            j = i+1;
+            sz = 1;
+            string line = words[i];
+            while(j<words.size() && cnt + sz + words[j].length()<=L) {
+                cnt += words[j].length();
+                ++j;
+                ++sz;
+            }
+            if(sz==1) {
+                line.append(L-line.length(), ' ');
+            } else {
+                if(j>=words.size()) {
+                    for(int k = 0; k<sz; ++k) {
+                        if(k)
+                            line+=words[i+k];
+                        line+=' ';
+                    }
+                    line.append(L-line.length(), ' ');
+                    ans.push_back(line);
+                    break;
+                }
+                quotient = (L-cnt)/(sz-1);
+                left = (L-cnt)%(sz-1);
+                for(int k = 0; k<sz; ++k) {
+                    if(k)
+                        line += words[i+k];
+                    if(k<sz-1)
+                        line.append(quotient, ' ');
+                    if(left) {
+                        line += ' ';
+                        --left;
+                    }
+                }
+            }
+            ans.push_back(line);
+            i = j;
+        }
+        return ans;
+    }
+};
