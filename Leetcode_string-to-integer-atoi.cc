88f6ba3c119663bea16d55f30362c4d3298e5ee9
axegang <fibonacci@zju.edu.cn>
2014-10-18 11:16:34 -0400

    update ANOTHER SOLUTION of string-to-integer-atoi

--- Leetcode_string-to-integer-atoi.cc
+++ Leetcode_string-to-integer-atoi.cc
@@ -46,3 +46,39 @@ public:
         return (int)(flag ? res : -res);
     }
 };
+
+//ANOTHER SOLUTION
+class Solution {
+public:
+    int atoi(const char *str) {
+        if(!str || !strlen(str))
+            return 0;
+        while(*str==' ')
+            ++str;
+        bool isPositive = true;
+        if(*str=='-') {
+            isPositive = false;
+            ++str;
+        }
+        else if(*str=='+') {
+            ++str;
+        }
+        long long int ans = 0;
+        while(*str) {
+            if(*str<'0' || *str>'9')
+                break;
+            ans = ans*10 + *str - '0';
+            ++str;
+        }
+        if(isPositive) {
+            if(ans > INT_MAX)
+                return INT_MAX;
+            return ans;
+        } else {
+            if(ans-1 >= INT_MAX)
+                return INT_MIN;
+            else
+                return -ans;
+        }
+    }
+};
