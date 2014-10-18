ffcb60e870d74bdcb7f77610a04598afa60e11bf
axegang <fibonacci@zju.edu.cn>
2014-10-18 10:21:29 -0400

    update ANOTHER SOLUTION of median-of-two-sorted-arrays

--- Leetcode_median-of-two-sorted-arrays.cc
+++ Leetcode_median-of-two-sorted-arrays.cc
@@ -27,3 +27,29 @@ public:
             return 0.5 * (find_kth_num(A, m, B, n, (m+n)/2) + find_kth_num(A, m, B, n, (m+n)/2+1));
     }
 };
+
+//Another solution
+class Solution {
+private:
+    double find(int A[], int m, int B[], int n, int k) {
+        if(m > n)
+            return find(B, n, A, m, k);
+        if(!m)
+            return B[k-1];
+        if(k==1)
+            return min(A[0], B[0]);
+            
+        int x = min(k/2, m);
+        if(A[x-1] < B[x-1])
+            return find(A+x, m-x, B, n, k-x);
+        else
+            return find(A, m, B+x, n-x, k-x);
+    }
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        if((m+n)%2)
+            return find(A, m, B, n, (m+n)/2+1);
+        else
+            return 0.5*(find(A, m, B, n, (m+n)/2) + find(A, m, B, n, (m+n)/2+1));
+    }
+};
