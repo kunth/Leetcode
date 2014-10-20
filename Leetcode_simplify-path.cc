0d00878e5ab199c0f85a60b2e65c1834ea029e97
axegang <fibonacci@zju.edu.cn>
2014-10-21 00:05:16 +0800

    update simplify-path

--- Leetcode_simplify-path.cc
+++ Leetcode_simplify-path.cc
@@ -35,3 +35,54 @@ public:
         return ans;
     }
 };
+
+//Another Solution
+class Solution {
+private:
+    vector<string> split(string str){
+        vector<string>ans;
+        int begin = 0;
+        for(int i = 0; i<str.length(); ++i) {
+            if(str[i]=='/') {
+                if(i>begin)
+                    ans.push_back(str.substr(begin, i-begin));
+                begin = i+1;
+            }
+        }
+        if(begin<str.length())
+            ans.push_back(str.substr(begin));
+        return ans;
+    }
+public:
+    string simplifyPath(string path) {
+        if(path.empty())
+            return path;
+        stack<string>st, rst;
+        vector<string>vec = split(path);
+        for(int i = 0; i<vec.size(); ++i) {
+            if(vec[i]=="..") {
+                if(!st.empty())
+                    st.pop();
+            } else if(vec[i]==".") {
+                continue;
+            } else {
+                st.push(vec[i]);
+            }
+        }
+        string ans;
+        if(st.empty()){
+            ans = "/";
+        } else {
+            while(!st.empty()) {
+                rst.push(st.top());
+                st.pop();
+            }
+            while(!rst.empty()) {
+                ans += "/";
+                ans += rst.top();
+                rst.pop();
+            }
+        }
+        return ans;
+    }
+};
