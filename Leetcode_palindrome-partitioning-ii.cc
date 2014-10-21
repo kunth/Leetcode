ac1817d517a251d7f928e2114e7ea441297e43e9
axegang <fibonacci@zju.edu.cn>
2014-10-21 21:25:41 +0800

    update palindrome-partitioning-ii

--- Leetcode_palindrome-partitioning-ii.cc
+++ Leetcode_palindrome-partitioning-ii.cc
@@ -28,3 +28,27 @@ public:
         return dp[len];
     }
 };
+
+//Another Solution
+class Solution {
+public:
+    int minCut(string s) {
+        int len = s.length();
+        if(len<=1)
+            return 0;
+        vector<vector<bool>>map(len+1, vector<bool>(len+1, false));
+        vector<int>dp(len+1);
+        for(int i = 0; i<=len; ++i)
+            dp[i]=i;
+
+        for(int i = 0; i<len; ++i) {
+            for(int j = 0; j<=i; ++j) {
+                if(s[i]==s[j] && (i-j<2 || map[j+1][i-1])) {
+                    map[i][j] = map[j][i] = true;
+                    dp[i+1] = min(dp[i+1], 1+dp[j]);
+                }
+            }
+        }
+        return dp[len]-1;
+    }
+};
