5ca02b65c112b6f510b5f25a2ef0f72b0465dfa8
axegang <fibonacci@zju.edu.cn>
2014-10-22 17:48:49 -0400

    update wildcard-matching

--- Leetcode_wildcard-matching.cc
+++ Leetcode_wildcard-matching.cc
@@ -1,25 +1,20 @@
 class Solution {
 public:
     bool isMatch(const char *s, const char *p) {
-        const char *copyp = NULL, *copys = s;
-        while(*s)
-        {
-            if(*s==*p || *p=='?')
-            {
+        if(!s && !p)
+            return true;
+        const char *cs = NULL, *cp = NULL;
+        while(*s) {
+            if(*s==*p || *p == '?'){
                 ++s;
                 ++p;
-            }
-            else if(*p=='*')
-            {
-                copyp = p++;
-                copys = s;
-            }
-            else if(copyp)
-            {
-                p = copyp+1;
-                s = ++copys;
-            }
-            else
+            } else if(*p=='*') {
+                cp = p++;
+                cs = s;
+            } else if(cp) {
+                p = cp+1;
+                s = ++cs;
+            } else
                 return false;
         }
         while(*p && *p=='*')
